0100e0c071018fe1751b052d53c43d66b9f265c3
Okwudili Twitter <122351230+okwudili-twitter@users.noreply.github.com>
2023-11-04 22:56:42 +0100

    Use of props and accepting data into component

--- js/components/MyButton.js
+++ js/components/MyButton.js
@@ -1,13 +1,30 @@
 export default {
     template : `
-        <button class="bg-gray-200 hover:bg-gray-400 border rounded p-1 px-2 disabled:cursor-not-allowed " :disabled="processing" >
+        <button
+            :class="{
+                'border rounded p-1 px-2 disabled:cursor-not-allowed' : true,
+                'bg-gray-200 hover:bg-gray-400' : btn_type === 'muted',
+                'bg-blue-200 hover:bg-blue-400' : btn_type === 'primary',
+                'bg-yellow-200 hover:bg-yellow-400' : btn_type === 'warning',
+                'bg-green-200 hover:bg-green-400' : btn_type === 'success',
+                'bg-red-200 hover:bg-red-400' : btn_type === 'danger',
+                'is_loading' : processing
+            }"
+
+            :disabled="processing"
+        >
             <slot />
         </button>
     `,
 
-    data() {
-        return {
-            processing : false
+    props : {
+        btn_type : {
+            type: String,
+            default: 'primary',
+        },
+        processing: {
+            type: Boolean,
+            default: false,
         }
-    }
+    },
 }
