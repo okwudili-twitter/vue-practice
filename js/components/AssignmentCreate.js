f158ec353ebe79f60e54c9e0ed6daeb818d83130
Okwudili Twitter <122351230+okwudili-twitter@users.noreply.github.com>
2023-11-05 10:16:26 +0100

    Added tag option to create assigment

--- js/components/AssignmentCreate.js
+++ js/components/AssignmentCreate.js
@@ -5,13 +5,35 @@ export default {
                 <input v-model="new_assignment" required placeholder="New Assignment..." class="p-2" />
                 <button type="submit" class="bg-white p-2 border-l">Add</button>
             </div>
+
+            <p>Tags:</p>
+
+            <div class="flex flex-wrap space-x-4" style="width: 250px;">
+
+                <label v-for="tag in tags" :key="tag.id" class="flex items-center space-x-2">
+                    <input
+                        type="checkbox"
+                        :value="tag.name"
+                        :name="tag.name"
+                        class="form-checkbox text-indigo-600 "
+                        v-model="new_tags"
+
+                    />
+                    <span>{{ tag.name }}</span>
+                </label>
+
+            </div>
+
             <small v-show="error" class="text-red-500" >{{ error }}</small>
+            <small v-show="noTagSelected" class="text-red-500" >Select at least 1 (one) tag!</small>
         </form>
     `,
 
     data() {
         return {
-            'new_assignment' : ''
+            'new_assignment' : '',
+            'new_tags' : [],
+            'noTagSelected' : false
         }
     },
 
@@ -21,13 +43,24 @@ export default {
         error : {
             type: String,
             default: ''
+        },
+        tags: {
+            type: Array
         }
     },
 
     methods: {
         add() {
-            this.$emit('add', this.new_assignment);
-            this.new_assignment = ''
+            if (this.new_tags.length === 0) {
+                // No tag is selected, show an error message.
+                this.noTagSelected = true;
+            } else {
+                this.noTagSelected = false;
+                this.$emit('add', this.new_assignment, this.new_tags);
+                this.new_assignment = '';
+                this.new_tags = [];
+            }
+
         }
     }
 
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -12,7 +12,7 @@ export default {
             <assignment-list title="In Progress" :assignments="filters.inProgress"></assignment-list>
             <assignment-list title="Completed" :assignments="filters.completed"></assignment-list>
 
-            <assignment-create @add="add" :error="error" ></assignment-create>
+            <assignment-create :tags="tags" @add="add" :error="error" ></assignment-create>
 
         </section>
 
@@ -21,10 +21,17 @@ export default {
     data() {
         return {
             assignments : [
-                {id:1, name: 'Finish Project 1', completed: false},
-                {id:2, name: 'Finish Project 2', completed: true},
-                {id:3, name: 'Finish Project 3', completed: false},
-                {id:4, name: 'Finish Project 4', completed: false},
+                {id:1, name: 'Finish Project 1', completed: false, tags: ['maths']},
+                {id:2, name: 'Finish Project 2', completed: true, tags: ['maths', 'science']},
+                {id:3, name: 'Finish Project 3', completed: false, tags: ['english', 'science', 'maths']},
+                {id:4, name: 'Finish Project 4', completed: false, tags: ['science']},
+            ],
+            tags : [
+                {id: 1, name: 'maths'},
+                {id: 2, name: 'english'},
+                {id: 3, name: 'science'},
+                {id: 4, name: 'fiction'},
+                {id: 5, name: 'Coding'},
             ],
             error: '',
         }
@@ -40,7 +47,7 @@ export default {
     },
 
     methods: {
-        add(name) {
+        add(name, tags) {
             // Check if the new_assignment already exists in assignments
             const assignmentExists = this.assignments.some(
                 (assignment) => assignment.name.toLowerCase() === name
@@ -52,7 +59,8 @@ export default {
                 this.assignments.push({
                     name: this.capitalizeString(name),
                     completed: false,
-                    id: this.assignments.length + 1
+                    id: this.assignments.length + 1,
+                    tags: tags,
                 });
 
                 this.error = '';
