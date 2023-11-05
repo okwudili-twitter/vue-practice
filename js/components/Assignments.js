c1739ed08c468fc041d6427e1f9f902486ee436a
Okwudili Twitter <122351230+okwudili-twitter@users.noreply.github.com>
2023-11-05 20:44:30 +0100

    Get the assignments and tags from an api

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -20,19 +20,8 @@ export default {
 
     data() {
         return {
-            assignments : [
-                {id:1, name: 'Finish Project 1', completed: false, tags: ['maths']},
-                {id:2, name: 'Finish Project 2', completed: true, tags: ['maths', 'science']},
-                {id:3, name: 'Finish Project 3', completed: false, tags: ['english', 'science', 'maths']},
-                {id:4, name: 'Finish Project 4', completed: false, tags: ['science']},
-            ],
-            tags : [
-                {id: 1, name: 'maths'},
-                {id: 2, name: 'english'},
-                {id: 3, name: 'science'},
-                {id: 4, name: 'fiction'},
-                {id: 5, name: 'coding'},
-            ],
+            assignments : [],
+            tags : [],
             error: '',
         }
     },
@@ -44,6 +33,20 @@ export default {
                 completed: this.assignments.filter(a => a.completed),
             }
         }
+    },
+
+    created() {
+        fetch('http://localhost:3000/assignments')
+            .then(response => response.json())
+            .then(assignments => {
+                this.assignments = assignments;
+            });
+
+        fetch('http://localhost:3000/tags')
+            .then(response => response.json())
+            .then(tags => {
+                this.tags = tags;
+            });
     },
 
     methods: {
